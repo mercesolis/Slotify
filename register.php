04f7fca2d18539a3b5952042bef0056c1801df53
mercesolis <mersolis@live.com>
2020-11-08 17:34:20 -0600

    login form

--- register.php
+++ register.php
@@ -13,7 +13,19 @@
         <div id="inputContainer">
             <form id="loginForm" action="register.php" method="POST">
                 <h2>Login to your account</h2>
-                <input id="loginUsername" name="loginUsername" type="text" placeholder="email address">
+                <p>
+                    <label for="loginUsername">Username</label>
+                    <input id="loginUsername" name="loginUsername" type="text" placeholder="email address" required>
+
+                </p>
+                <p>
+                    <label for="loginPassword">Password</label>
+                    <input id="loginPassword" name="loginPassword" type="password" required>
+                </p>
+
+                <button type="submit" name="loginButton">LOG IN</button>
+
+
             </form>
         </div>
     </body>
